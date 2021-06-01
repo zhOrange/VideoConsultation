6cf091d50e3b81053c152c239b87c95cc619a9c4
Bug_Fly <x_lemon_zh@163.com>
2021-06-01 23:00:34 +0800

    错的，哈哈哈

--- app/src/main/java/com/zh/videoconsultation/ConsultActivity.java
+++ app/src/main/java/com/zh/videoconsultation/ConsultActivity.java
@@ -4,10 +4,14 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -16,8 +20,21 @@ import android.widget.Toast;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.zh.videoconsultation.utils.DensityUtil;
+
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import com.zh.videoconsultation.lib.adapter.ScrollPickerAdapter;
+import com.zh.videoconsultation.lib.view.ScrollPickerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ConsultActivity extends AppCompatActivity {
+    private ScrollPickerView mScrollPickerViewWeek;
+    private ScrollPickerView mScrollPickerViewDuration;
 
     private Button btnOrder, btnCall;
     private FloatingActionButton fbtnMark;
@@ -29,6 +46,7 @@ public class ConsultActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_consult);
+
 
 
         Intent intent = getIntent();
@@ -56,8 +74,10 @@ public class ConsultActivity extends AppCompatActivity {
             public void onClick(View view) {
                 btnOrder.setEnabled(false);
                 btnCall.setEnabled(true);
-                Intent intent = new Intent(ConsultActivity.this, ScrollPickerDemoActivity.class);
-                startActivity(intent);
+//                Intent intent = new Intent(ConsultActivity.this, ScrollPickerDemoActivity.class);
+//                startActivity(intent);
+
+                show1();
                 Toast.makeText(ConsultActivity.this, "成功预约 " + consultName + "顾问！", Toast.LENGTH_SHORT).show();
 
             }
@@ -67,6 +87,7 @@ public class ConsultActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Toast.makeText(ConsultActivity.this, "开始视频咨询", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(ConsultActivity.this, ChatActivity.class);
+//                show2();
                 startActivity(intent);
             }
         });
@@ -105,5 +126,102 @@ public class ConsultActivity extends AppCompatActivity {
                 break;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    private void show1() {
+
+        Dialog bottomDialog = new Dialog(this, R.style.BottomDialog);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.dialog_choose_bottom, null);
+        bottomDialog.setContentView(contentView);
+        ViewGroup.LayoutParams layoutParams = contentView.getLayoutParams();
+        layoutParams.width = getResources().getDisplayMetrics().widthPixels;
+        contentView.setLayoutParams(layoutParams);
+        bottomDialog.getWindow().setGravity(Gravity.BOTTOM);
+        bottomDialog.getWindow().setWindowAnimations(R.style.BottomDialog_Animation);
+
+        bottomDialog.show();
+        initView();
+        initData();
+    }
+
+    private void show2() {
+        Dialog bottomDialog = new Dialog(this, R.style.BottomDialog);
+        View contentView = LayoutInflater.from(this).inflate(R.layout.dialog_content_circle, null);
+        bottomDialog.setContentView(contentView);
+        ViewGroup.MarginLayoutParams params = (ViewGroup.MarginLayoutParams) contentView.getLayoutParams();
+        params.width = getResources().getDisplayMetrics().widthPixels - DensityUtil.dp2px(ConsultActivity.this, 16f);
+        params.bottomMargin = DensityUtil.dp2px(ConsultActivity.this, 8f);
+        contentView.setLayoutParams(params);
+        bottomDialog.getWindow().setGravity(Gravity.BOTTOM);
+        bottomDialog.getWindow().setWindowAnimations(R.style.BottomDialog_Animation);
+        bottomDialog.show();
+    }
+
+    private void initView() {
+        mScrollPickerViewWeek = findViewById(R.id.scroll_picker_view_week_1);
+//        mScrollPickerViewWeek = findViewById(R.id.scroll_picker_view_week_1);
+        mScrollPickerViewDuration = findViewById(R.id.scroll_picker_view_duration_1);
+        if(mScrollPickerViewWeek != null){
+            mScrollPickerViewWeek.setLayoutManager(new LinearLayoutManager(this));
+        }
+        mScrollPickerViewDuration.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    private void initData() {
+        List<String> weekList, durationList;
+        String[] weeks = {"周一", "周二","周三","周四","周五","周六","周日"};
+        String[] durations =   {"08:00 ~ 08.30", "08:30 ~ 09.00",
+                "09:00 ~ 09.30", "09:30 ~ 10.00",
+                "10:00 ~ 10.30", "10:30 ~ 11.00",
+                "11:00 ~ 11.30", "11:30 ~ 12.00",
+                "12:00 ~ 12.30", "12:30 ~ 13.00",
+                "13:00 ~ 13.30", "13:30 ~ 14.00",
+                "14:00 ~ 14.30", "14:30 ~ 15.00",
+                "05:00 ~ 15.30", "15:30 ~ 16.00",
+                "16:00 ~ 16.30", "16:30 ~ 17.00",
+                "17:00 ~ 17.30", "17:30 ~ 18.00"};
+        weekList = Arrays.asList(weeks);
+        durationList = Arrays.asList(durations);
+//        for (int i = 0; i < 20; i++) {
+//            String itemData = "item: " + i;
+//            weekList.add(itemData);
+//        }
+
+        ScrollPickerAdapter.ScrollPickerAdapterBuilder<String> weekBuilder =
+                new ScrollPickerAdapter.ScrollPickerAdapterBuilder<String>(this)
+                        .setDataList(weekList)
+                        .selectedItemOffset(1)
+                        .visibleItemNumber(3)
+                        .setDivideLineColor("#E5E5E5")
+                        .setItemViewProvider(null)
+                        .setOnClickListener(new ScrollPickerAdapter.OnClickListener() {
+                            @Override
+                            public void onSelectedItemClicked(View v) {
+                                String text = (String) v.getTag();
+                                if (text != null) {
+                                    Toast.makeText(ConsultActivity.this, text, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+        ScrollPickerAdapter.ScrollPickerAdapterBuilder<String> durationBuilder =
+                new ScrollPickerAdapter.ScrollPickerAdapterBuilder<String>(this)
+                        .setDataList(durationList)
+                        .selectedItemOffset(1)
+                        .visibleItemNumber(3)
+                        .setDivideLineColor("#E5E5E5")
+                        .setItemViewProvider(null)
+                        .setOnClickListener(new ScrollPickerAdapter.OnClickListener() {
+                            @Override
+                            public void onSelectedItemClicked(View v) {
+                                String text = (String) v.getTag();
+                                if (text != null) {
+                                    Toast.makeText(ConsultActivity.this, text, Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+        ScrollPickerAdapter mScrollPickerAdapterWeek = weekBuilder.build();
+        ScrollPickerAdapter mScrollPickerAdapterDuration = durationBuilder.build();
+        mScrollPickerViewWeek.setAdapter(mScrollPickerAdapterWeek);
+        mScrollPickerViewDuration.setAdapter(mScrollPickerAdapterDuration);
     }
 }
